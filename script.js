90361b4ed9434c9ac192842369de03ccd6b8b538
Molly Thornton <mmthornto@gmail.com>
2019-10-09 17:44:04 +0000

    working api

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const url = "https://helloacm.com/api/"
-fetch(url)
-    .then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        console.log(json);
+document.getElementById("getFortune").addEventListener("click", async function(event) {
+  event.preventDefault();
+var url = "https://cors-anywhere.herokuapp.com";
+  url +="/quote-garden.herokuapp.com/quotes/random";
+  //steakovercooked.com/api/fortune";
 
-    })
-//idk im bad at api
+  console.log(url);
+  //const response = await fetch(url);
+  //let JSON=await response.json();
+  //alert("here");
+  //console.log("response",response.JSON);
+ fetch(url, { mode: 'cors' })
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      console.log(json);
+      document.getElementById("fortune").innerHTML = json.quoteText;
+    });
+})
